6a0e2824c5c4f58370cdfc3283b25927a860b2a9
chrisgarcia001 <chrisgarcia001@gmail.com>
2017-09-18 14:50:32 -0400

    Updating code

--- basic_functions.py
+++ basic_functions.py
@@ -6,6 +6,24 @@ def add_2(x, y):
 	
 # Illustrate default arguments:
 def my_range(start, end, by=1):
-	return range(start, end, by)
+	# return range(start, end, by)
 	# Homework: rewrite this function to use a for loop rather than resorting to Python's builtin range function.
-	
+	rng = []
+	next_val = start
+	while next_val <= end:
+		rng.append(next_val)
+		next_val += by
+	return rng
+
+# Prints a triangle of the specified length/height n.
+# If full == True, n is the height.	
+def print_triangle(n, full=False):
+	pos = 1
+	while pos <= n:
+		print('*' * pos)
+		pos += 1
+	if full:
+		pos = n - 1
+		while pos >= 1:
+			print('*' * pos)
+			pos -= 1
--- test_bs.py
+++ test_bs.py
@@ -12,3 +12,11 @@ print(bs.add_2(7, 8))
 print(bs.my_range(1, 50))
 print(bs.my_range(1, 50, 3))
 print(bs.my_range(1, 50, by=4)) # Note we can specify the optional arg. inputs
+print(bs.my_range(1, 11, by=3))
+
+# Test teh triangle-printing function.
+bs.print_triangle(3)
+print("\n\n")
+bs.print_triangle(5)
+print("\n\n")
+bs.print_triangle(5, full=True)
