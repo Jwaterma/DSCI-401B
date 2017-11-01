c57fa84c5c46827277fa96cd276de8b81ed48c39
chrisgarcia001 <chrisgarcia001@gmail.com>
2017-11-01 01:04:20 -0400

    Adding code examples

--- cars_basic_reg.py
+++ cars_basic_reg.py
@@ -9,6 +9,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import median_absolute_error
 from sklearn.metrics import r2_score
 from sklearn.metrics import explained_variance_score
+from sklearn.feature_selection import SelectPercentile
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
 
 cars = pd.read_csv('./data/cars.csv.')
 
@@ -59,5 +62,22 @@ print('MSE, MAE, R^2, EVS: ' + str([mean_squared_error(y_test, preds), \
 							   r2_score(y_test, preds), \
 							   explained_variance_score(y_test, preds)])) 
 
-	
+# ---------------- Part 3: Perform variable selection to arrive at the best model --
+
+# Create a least squares linear regression model.
+model = linear_model.LinearRegression()
+
+# Split training and test sets from main set. Note: random_state just enables results to be repeated.
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.2, random_state = 4)
+
+# Fit the model.
+model.fit(x_train,y_train)
+
+# Make predictions on test data and look at the results.
+preds = model.predict(x_test)
+pprint.pprint(pd.DataFrame({'Actual':y_test, 'Predicted':preds}))
+print('MSE, MAE, R^2, EVS: ' + str([mean_squared_error(y_test, preds), \
+							   median_absolute_error(y_test, preds), \
+							   r2_score(y_test, preds), \
+							   explained_variance_score(y_test, preds)])) 	
 						   
